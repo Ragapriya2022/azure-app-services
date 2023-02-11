aebd3275a6fb5a37e0616c27e29777bbed628429
Ragapriya <ragapriya2022@outlook.com>
2023-02-11 16:04:40 +0000

    commit

---
+++ scripts.sh
@@ -0,0 +1,16 @@
+az ad sp create-for-rbac --name "webappraga" --role contributor --scopes /subscriptions/70d4d8f2-9121-43a2-a138-6dbb5d661809 --sdk-auth
+
+
+{
+  "clientId": "a83d6c9b-62a5-4590-871a-a3b06dc53580",
+  "clientSecret": "-i98Q~lJMRpPkBzTDmmQYc9plaKeCePDW0whLdpx",
+  "subscriptionId": "70d4d8f2-9121-43a2-a138-6dbb5d661809",
+  "tenantId": "f6995460-78d3-4278-a1d6-ea0cd0a6280c",
+  "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+  "resourceManagerEndpointUrl": "https://management.azure.com/",
+  "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+  "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+  "galleryEndpointUrl": "https://gallery.azure.com/",
+  "managementEndpointUrl": "https://management.core.windows.net/"
+}
+
